8aa6a8c21e06f5eec6c38bfaea90a16337750492
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-04-16 00:28:20 +1000

    cport change

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, () =>{
-    console.log('Server started on port 3000');
+app.listen(8080, () =>{
+    console.log('Server started on port 8080');
 });
