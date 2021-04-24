e422303383c55e42de82fd43fa9d86d3e9e935f6
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-04-25 02:49:00 +1000

    styled home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     console.log('Home page running');
-    $(".dropdown-trigger").dropdown();
+    $(".dropdown-trigger").dropdown(); //dropdown 
 
 });
-
+//side bar nav
 const sideNavigation = document.querySelector('.sidenav');
 M.Sidenav.init(sideNavigation, {});
 
+//home page slider 
 const slider = document.querySelector(".slider");
 M.Slider.init(slider, {
     indicator: false,
@@ -14,3 +15,12 @@ M.Slider.init(slider, {
     transition: 500,
     interval: 6000
 });
+
+//autocomplete list of cities in australia
+// const autocomp = document.querySelector('.autocomplete');
+// M.Autocomplete.init(autocomp, {
+//     data: {
+
+//     }
+// })
+
